793b749583d258a77aaca4519dd90357f59e92d9
trebsirk <trebsirk@gmail.com>
2024-08-23 02:16:16 -0400

    added parallel implementation to person.clj file reader

--- src/models/person.clj
+++ src/models/person.clj
@@ -1,11 +1,14 @@
 (ns models.person
-  (:require [clojure.string :as str]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
-;; (str/split "2,Alice,30" #",")
+;; (str/split "2,Alice," #"," -1)
 
-(defrecord Person [id name age])
+(defrecord Person [id ^String name age])
 
-(defn valid-person-name? [name]
+;; (->Person 1 "ty" nil)
+
+(defn valid-name? [name]
   (and (string? name) (not (empty? name))))
 
 (defn valid-age? [age]
@@ -17,12 +20,53 @@
     (catch NumberFormatException e
       nil)))
 
+(defn parse-name-valid-or-nil [name]
+  (cond
+    (not (valid-name? name)) nil
+    :else name))
+
+(defn parse-age-valid-or-nil [age]
+  (cond
+    (not (valid-age? age)) nil
+    :else age))
+
+(defn create-person [id name age]
+  (cond
+    (not (valid-name? name)) (throw (IllegalArgumentException. "Invalid name"))
+    (not (valid-age? age)) (throw (IllegalArgumentException. "Invalid age"))
+    :else (->Person id name (Integer. age))))
+
+(defn create-person-with-nils [id name age]
+  (->Person id (parse-name-valid-or-nil name) (parse-age-valid-or-nil age)))
+
 (defn parse-person [line]
   "Parses a line of text and returns a Person record."
-  (let [[id name age] (str/split line #",")]
-    (->Person id name (parse-int age))))
+  ;; (println line)
+  (let [[id name age] (str/split line #"," -1)]
+    (create-person-with-nils id name (parse-int age))))
 
 (defn read-persons-from-file [file-path]
   "Reads a file and converts its contents to a list of Person records."
-  (with-open [reader (clojure.java.io/reader file-path)]
+  (with-open [reader (io/reader file-path)]
     (doall (map parse-person (line-seq reader)))))
+
+(read-persons-from-file "resources/people.txt")
+
+;; parallel implementation
+(defn parse-line [line]
+  (let [[id name-str age-str] (str/split line #"," -1)
+        name (cond (not (valid-name? name-str)) nil :else name-str)
+        age (try (Integer/parseInt age-str) (catch Exception _ nil))]
+    (->Person id name age)))
+
+(defn parse-lines [lines]
+  (map parse-line lines))
+
+(defn read-persons-from-file-parallel [filename chunk-size]
+  (let [lines (line-seq (io/reader filename))
+        chunks (partition-all chunk-size lines)]
+    (->> chunks
+         (pmap parse-lines)
+         (apply concat))))
+
+(read-persons-from-file-parallel "resources/people.txt" 4)
